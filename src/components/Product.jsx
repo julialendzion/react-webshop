e388d4b15626601f93e02718d8ef7a23edb48489
Julia Lendzion <juli47g3@stud.kea.dk>
2021-11-16 13:05:23 +0100

    states

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Product(props) {
   const styles = {
     border: "2px solid #000",
@@ -5,10 +7,24 @@ export default function Product(props) {
     textAlign: "center",
     backgroundColor: props.soldOut ? "red" : "lightgreen",
   };
+  const initial = 0;
+  const [stateVariable, updaterFunction] = useState(initial);
+
+  function addValue() {
+    updaterFunction((prevCount) => prevCount + 1);
+  }
+
+  function removeValue() {
+    updaterFunction((prevCount) => prevCount - 1);
+  }
+
   return (
     <article style={styles}>
       <h2>{props.productdisplayname}</h2>
       <p>{props.price}</p>
+      <button onClick={addValue}> + </button>
+      <p>{stateVariable}</p>
+      <button onClick={removeValue}> - </button>
     </article>
   );
 }
