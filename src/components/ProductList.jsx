53b19f58c0e911c8727e8c06d41e28c8035c0f37
Julia Lendzion <juli47g3@stud.kea.dk>
2021-11-16 15:25:48 +0100

    styling

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,7 @@
 import Product from "./Product";
 
 export default function ProductList(props) {
-  const mapped = props.products.map((product) => <Product {...product} />);
+  const mapped = props.products.map((product) => <Product key={product.id} {...product} />);
 
   return <section className="ProductList">{mapped}</section>;
 }
